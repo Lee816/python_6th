19d0925ec1c23e76f46c2a808ecc18d0f9e0898a
Lee816 <lgha0816@naver.com>
2023-05-30 13:23:15 +0900

    문자열 연결하기

--- 230530index.js
+++ 230530index.js
@@ -1,39 +1,54 @@
-var name = "이승훈";
-var age = 29;
-var cgpa = 3.92;
-var lineBreak = "<br/>";
+var lastName = "홍";
+var firstName = "길동";
+var fullName = lastName+firstName;
 
-document.write("이름 : " + name + lineBreak);
-document.write("나이 : " + age + lineBreak);
-document.write("학점 : " + cgpa + lineBreak);
+console.log(fullName);
+console.log("Today is"+" a "+"beautiful day");
+console.log("My name is "+fullName);
+
+var num1 = 20;
+var num2 = 30;
+var sum = num1 + num2;
+console.log(num1 + num2);
+console.log(""+num1 + num2);
+console.log(num1+ " + " + num2 + " = " + sum);
+
+// var name = "이승훈";
+// var age = 29;
+// var cgpa = 3.92;
+// var lineBreak = "<br/>";
+
+// document.write("이름 : " + name + lineBreak);
+// document.write("나이 : " + age + lineBreak);
+// document.write("학점 : " + cgpa + lineBreak);
 
 
-console.log(123, typeof 123);
-console.log(123.5, typeof 123.5);
-console.log('123', typeof '123');
-console.log(true, typeof true);
-console.log(false, typeof false);
+// console.log(123, typeof 123);
+// console.log(123.5, typeof 123.5);
+// console.log('123', typeof '123');
+// console.log(true, typeof true);
+// console.log(false, typeof false);
 
-var car;
-console.log(car);
-var car = "";
-console.log(car);
+// var car;
+// console.log(car);
+// var car = "";
+// console.log(car);
 
-var person = {firstName : 'John', lastName : "Doe", age : 50, eyeColor : "blue"};
-console.log(typeof person);
-person = null;
+// var person = {firstName : 'John', lastName : "Doe", age : 50, eyeColor : "blue"};
+// console.log(typeof person);
+// person = null;
 
-console.log(typeof person);
+// console.log(typeof person);
 
-document.write('Hello World');
-document.write('Hello World');
-document.write('<h1>Welcome to JS Program</h1>');
-document.write('<h2>Welcome to JS Program</h2>');
+// document.write('Hello World');
+// document.write('Hello World');
+// document.write('<h1>Welcome to JS Program</h1>');
+// document.write('<h2>Welcome to JS Program</h2>');
 
-console.log('Welcome JS Program');
-console.warn('Welcome JS Program');
-console.error('Welcome JS Program');
+// console.log('Welcome JS Program');
+// console.warn('Welcome JS Program');
+// console.error('Welcome JS Program');
 
-alert('Welcome JS Program');
-var a = prompt('Welcom JS Program');
-console.log(a);
+// alert('Welcome JS Program');
+// var a = prompt('Welcom JS Program');
+// console.log(a);
