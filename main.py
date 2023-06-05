d50b1fe1ddd2c1f73a2b886286ed7e335c379566
Lee816 <lgha0816@naver.com>
2023-06-05 11:30:42 +0900

    함수 연산자 실습

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-print("Hello World")
+# 변수 선언
+a = 10
+b = 3.14
+c = "Hello, World!"
+d = [1, 2, 3]
+e = (4, 5, 6)
+f = {7, 8, 9}
+g = {"apple":1 ,"banana":2, "orange":3}
+
+# 데이터 타입 출력
+print("a: ",type(a))
+print("b: ",type(b))
+print("c: ",type(c))
+print("d: ",type(d))
+print("e: ",type(e))
+print("f: ",type(f))
+print("g: ",type(g))
+
+# 덧셈
+a = 4
+b = 2
+total = a + b
+print('totla = a + b', total)
+
+# 뺄셈
+total = a - b
+print('totla = a - b', total)
+
+# 곱셈
+total = a * b
+print('totla = a * b', total)
+
+# 나눗셈
+total = a / b
+print('totla = a / b', total)
+
+# 나머지
+a = 5
+b = 2
+print('a % b :', a%b)
+
+# 거듭제곱
+print('a ** b:',a**b)
+
+# 몫(양수)
+print('a//b: ',a//b)
+
+# 몫(음수)
+a = -5
+print('a//b: ',a//b)
