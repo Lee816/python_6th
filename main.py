62a83866f938585a55338d25a58280ac27377826
Lee816 <lgha0816@naver.com>
2023-06-05 13:19:33 +0900

    논리 연산자

--- main.py
+++ main.py
@@ -20,27 +20,27 @@ print("g: ",type(g))
 a = 4
 b = 2
 total = a + b
-print('totla = a + b', total)
+print('totla = a+b = ', total)
 # 뺄셈
 total = a - b
-print('totla = a - b', total)
+print('totla = a-b = ', total)
 # 곱셈
 total = a * b
-print('totla = a * b', total)
+print('totla = a*b = ', total)
 # 나눗셈
 total = a / b
-print('totla = a / b', total)
+print('totla = a/b = ', total)
 # 나머지
 a = 5
 b = 2
-print('a % b :', a%b)
+print('a%b :', a%b)
 # 거듭제곱
-print('a ** b:',a**b)
+print('a**b : ',a**b)
 # 몫(양수)
-print('a//b: ',a//b)
+print('a//b : ',a//b)
 # 몫(음수)
 a = -5
-print('a//b: ',a//b)
+print('a//b : ',a//b)
 
 # 비교연산자
 a = 5
@@ -49,3 +49,16 @@ print('a<b : ',a<b)
 print('a<=b : ',a<=b)
 print('a==b : ',a==b)
 print('a!=b : ',a!=b)
+
+# 논리연산자
+a = 5
+b = 2
+c = 3
+d = 200
+
+print('a>b and a>c : ',a>b and a>c)
+print('a>b and a<c : ',a>b and a<c)
+print('a>b or a>c : ',a>b or a>c)
+print('a>b or a<c : ',a>b or a<c)
+print('a<b : ',a<b)
+print('not(a<b) : ',not(a<b))
