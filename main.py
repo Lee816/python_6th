fc233eea54cd079ef4a32bdedad2908487cee2ec
Lee816 <lgha0816@naver.com>
2023-06-05 13:11:02 +0900

    비교 연산자

--- main.py
+++ main.py
@@ -21,30 +21,31 @@ a = 4
 b = 2
 total = a + b
 print('totla = a + b', total)
-
 # 뺄셈
 total = a - b
 print('totla = a - b', total)
-
 # 곱셈
 total = a * b
 print('totla = a * b', total)
-
 # 나눗셈
 total = a / b
 print('totla = a / b', total)
-
 # 나머지
 a = 5
 b = 2
 print('a % b :', a%b)
-
 # 거듭제곱
 print('a ** b:',a**b)
-
 # 몫(양수)
 print('a//b: ',a//b)
-
 # 몫(음수)
 a = -5
 print('a//b: ',a//b)
+
+# 비교연산자
+a = 5
+b = 2
+print('a<b : ',a<b)
+print('a<=b : ',a<=b)
+print('a==b : ',a==b)
+print('a!=b : ',a!=b)
