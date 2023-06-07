ea4e4520f3b14eb43ce9ed99647dbb889875c23d
Lee816 <lgha0816@naver.com>
2023-06-07 15:24:18 +0900

    함수 실습 - 여러 반환 값 tuple

--- main.py
+++ main.py
@@ -573,3 +573,35 @@ def add(y):
 
 add(20.1597)
 
+def add():
+    x = 10
+    y = 20
+    c = x + y
+    return c
+
+sum1 = add()
+print(sum1)
+
+def add2():
+    x = 10
+    y = 20
+    return x+y
+
+sum2= add2()
+print(sum2)
+
+def add(y):
+    x = 10
+    return x+y
+
+sum3 = add(20)
+print(sum3)
+
+def add(y):
+    x = 10
+    c = x+y
+    d = y-x
+    return c,d, 50
+
+sum4, sub1, a = add(20)
+print(sum4, sub1, a)
