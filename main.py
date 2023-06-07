05db3115f3096efc923e2275de8212c15a853da6
Lee816 <lgha0816@naver.com>
2023-06-07 10:18:08 +0900

    while 루프 실습

--- main.py
+++ main.py
@@ -270,3 +270,59 @@ elif day == "Wed":
     print("수요일")
 else:
     print("휴일")
+
+# while 루프
+i = 0
+while i < 10:
+    i += 1
+    print("i :", i)
+    if a == 5:
+        break
+else: # while 문이 끝나면 실행 break로 탈출할 경우 실행 안됨
+    print("else")
+
+# 첫번째 while 루프
+a = 1
+while a <= 10:
+    print(a)
+    a += 1
+print("코드종료")
+
+a = 2
+while a <= 20:
+    print(a)
+    a += 2
+print("코드종료")
+
+a = 1
+while a <= 10:
+    print(a)
+    a += 1
+else:
+    print("While 조건이 거짓이므로 Else 부분 시행됨")
+print("코드종료")
+
+# 무한 루프
+
+# while True:
+#     print("멋쟁이 사자")
+# print("코드 종료")
+
+i = 0
+while True:
+    i += 1
+    print(i)
+    if i == 5:
+        break
+print("코드 종료")
+
+# while 중첩
+i = 1
+while i <= 3:
+    print("Outer Loop",i)
+    i += 1
+    j = 1
+    while j <= 5:
+        print('Inner Loop',j)
+        j += 1
+print('코드 종료')
