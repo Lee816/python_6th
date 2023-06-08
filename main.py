dd8ac700ac503b6aa16bfb87cdcf95dc4a2236c9
Lee816 <lgha0816@naver.com>
2023-06-08 13:12:44 +0900

    제너레이터 실습 2

--- main.py
+++ main.py
@@ -815,3 +815,16 @@ print('=======')
 
 for num in  runner:
     print(num)
+
+
+def generate_alphabet(start_letter, end_letter):
+    start = ord(start_letter)
+    end = ord(end_letter)
+    while start <= end:
+        yield chr(start)
+        start += 1
+
+runner = generate_alphabet('A','F')
+
+for letter in runner:
+    print(letter,end=' ')
