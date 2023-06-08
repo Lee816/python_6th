38c16d9be72f2ef582a2290c7d0f2c2180d24c79
Lee816 <lgha0816@naver.com>
2023-06-08 15:48:33 +0900

    데코레이터 실습

--- main.py
+++ main.py
@@ -1013,3 +1013,20 @@ add_sub = lambda x,y : (x+y,x-y)
 a,s = add_sub(5,2)
 print(a)
 print(s)
+
+def decor(fun):
+    def inner():
+        a = fun()
+        add = a + 5
+        return add
+    return inner
+
+def num():
+    return 10
+
+result_fun = decor(num)
+print(result_fun())
+
+@decor
+def num():
+    return 10
