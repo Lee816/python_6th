f7ea4c9a32948ce8a7cccabdf89a619e7e9d1631
Lee816 <lgha0816@naver.com>
2023-06-08 11:31:38 +0900

    사용자 입력 리스트

--- main.py
+++ main.py
@@ -788,3 +788,13 @@ print('numbers',numbers)
 
 numbers_clone = numbers[:]
 print(numbers_clone)
+
+# 사용자 입력으로 리스트 만들기
+user_input_list= []
+num_elements = int(input('Enter Number of Element : '))
+for i in range(num_elements):
+    user_input_list.append(input('Enter Element : '))
+
+print('User Input List: ')
+for element in user_input_list:
+    print(element,end=' ')
