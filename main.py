9bd04a2d15e206040c267d381872eafaa76a4c80
Lee816 <lgha0816@naver.com>
2023-06-09 13:42:25 +0900

    파일 포인터 위치 확인 및 변경

--- main.py
+++ main.py
@@ -1177,3 +1177,20 @@ content = 'This is a new file.\nPython is fun!'
 file_object.write(content)
 
 file_object.close()
+
+print('파일 열기')
+file_object = open('example.txt','r')
+
+print('현재 파일 포인터 위치 확인')
+position = file_object.tell()
+print('Current position : ',position)
+
+print('파일 포인터 위치 변경')
+file_object.seek(7)
+
+print('변경된 포인터 위치 확인')
+position = file_object.tell()
+print('New position',position)
+
+print('파일 닫기')
+file_object.close()
