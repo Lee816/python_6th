f5d500c1175beb12d080ee3654587777ca4e4d46
Lee816 <lgha0816@naver.com>
2023-06-12 13:02:45 +0900

    패키지 실습

--- main.py
+++ main.py
@@ -1394,18 +1394,21 @@ print(my_car.start_engine())
 # 다중상속
 class Engine:
     def start(self):
-        return 'Engine started'
+        return "Engine started"
 
     def stop(self):
-        return 'Engine stopped'
+        return "Engine stopped"
+
 
 class Wheels:
     def rotate(self):
         return "Wheels are rotating"
 
+
 # 다중상속
 class Car(Engine, Wheels):
     pass
+
 
 # 인스턴스 생성
 my_car = Car()
@@ -1413,3 +1416,8 @@ my_car = Car()
 # 부모 클래스의 메소드 사용
 print(my_car.start())
 print(my_car.rotate())
+
+from MyApp.Handlers.text_handler import handle_text
+
+input_text = "python package practice"
+handle_text(input_text)
