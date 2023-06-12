29bee9dc8591da3b44521c197f9bb5897fe7f741
Lee816 <lgha0816@naver.com>
2023-06-12 10:50:40 +0900

    다중상속 실습

--- main.py
+++ main.py
@@ -1389,3 +1389,27 @@ my_car = Car("Kia", "ForteCoup", 2013)
 
 # 메서드 호출
 print(my_car.start_engine())
+
+
+# 다중상속
+class Engine:
+    def start(self):
+        return 'Engine started'
+
+    def stop(self):
+        return 'Engine stopped'
+
+class Wheels:
+    def rotate(self):
+        return "Wheels are rotating"
+
+# 다중상속
+class Car(Engine, Wheels):
+    pass
+
+# 인스턴스 생성
+my_car = Car()
+
+# 부모 클래스의 메소드 사용
+print(my_car.start())
+print(my_car.rotate())
