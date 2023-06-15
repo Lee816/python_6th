4f44b9222bd1d37e10f06ecf2a215dda8317e11a
Lee816 <lgha0816@naver.com>
2023-06-15 09:29:34 +0900

    스택 구현 실습

--- main.py
+++ main.py
@@ -1543,3 +1543,43 @@ def timsort(arr):
 a = ["f", "g", "h", "z", "s", "b", "c", "d"]
 
 print(timsort(a))
+
+
+class ListNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def is_empty(self):  # 비어 있는지 확인
+        return self.size == 0
+
+    def push(self, value):  # 스택에서 값을 저장하는 방법
+        new_node = ListNode(value)
+        new_node.next = self.head
+        self.head = new_node
+        self.size += 1
+
+    def pop(self):  # 하나를 꺼내옴
+        if self.is_empty():  # 꺼낼 대상이 없을때 요류를 발생
+            raise IndexError("pop from an empty stack")
+        value = self.head.value
+        self.head = self.head.next
+        self.size -= 1
+        return value
+
+
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+stack.push(5)
+
+for _ in range(5):
+    print(stack.pop())
