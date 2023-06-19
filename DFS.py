60a3935b3147058322483daf5cefc069a9c68b56
Lee816 <lgha0816@naver.com>
2023-06-19 11:26:06 +0900

    DFS 순회

---
+++ DFS.py
@@ -0,0 +1,25 @@
+graph = {
+    "A": ["B", "C"],
+    "B": ["A", "D", "E"],
+    "C": ["A", "F"],
+    "D": ["B"],
+    "E": ["B", "F"],
+    "F": ["C", "E"],
+}
+
+visited = set()
+
+
+def dfs_iterative(start_node):
+    stack = [start_node]
+
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            print(node, end=" ")
+            visited.add(node)
+            stack.extend(reversed(graph[node]))
+
+
+first_node = "A"
+dfs_iterative(first_node)
