92ad47589fe8824fe6936af5e8a2e5d98654be89
Lee816 <lgha0816@naver.com>
2023-06-19 15:32:54 +0900

    웹페이지 이미지 다운로더 실습

--- html_parser.py
+++ html_parser.py
@@ -1,4 +1,7 @@
-from urllib.request import urlopen
+import http
+from pathlib import Path
+from urllib.parse import urljoin, urlunparse
+from urllib.request import urlopen, urlretrieve
 from html.parser import HTMLParser
 
 
@@ -24,7 +27,23 @@ def parse_image(data):
     return data_set
 
 
-def main():
+def download_image(url, data):
+    download_dir = Path("DOWNLOAD")
+    download_dir.mkdir(exist_ok=True)
+
+    parser = ImageParser()
+    parser.feed(data)
+    data_set = set(x for x in parser.result)
+    for x in sorted(data_set):
+        image_url = urljoin(url, x)
+        basename = Path(image_url).name
+        target_file = download_dir / basename
+        print(target_file)
+        print("Downloading...", image_url)
+        urlretrieve(image_url, target_file)
+
+
+def main1():
     url = "https://google.co.kr"
     with urlopen(url) as f:
         charset = f.headers.get_params("charset")[1][1]
@@ -35,5 +54,22 @@ def main():
     print("\n".join(sorted(data_set)))
 
 
+def main2():
+    host = "www.google.co.kr"
+    connection = http.client.HTTPConnection(host)
+
+    connection.request("GET", "")
+    response = connection.getresponse()
+
+    charset = response.msg.get_param("charset")
+    print("charset :", charset)
+    data = response.read().decode(charset)
+    connection.close()
+
+    print("\n>>>>> Downloading...", host)
+    url = urlunparse(("http", host, "", "", "", ""))
+    download_image(url, data)
+
+
 if __name__ == "__main__":
-    main()
+    main2()
