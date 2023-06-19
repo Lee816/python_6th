7cde376642deb23b2c03be36e29959c22022fc19
Lee816 <lgha0816@naver.com>
2023-06-19 15:54:00 +0900

    wsgiref 실습

---
+++ python/wsgiref_server.py
@@ -0,0 +1,16 @@
+from wsgiref.simple_server import make_server
+
+
+def application(environ, start_response):
+    response_body = b"Hello, World!!"
+    status = "200 OK"
+    headers = [("Content-Type", "text/plain")]
+
+    start_response(status, headers)
+    return [response_body]
+
+
+if __name__ == "__main__":
+    httpd = make_server("", 8000, application)
+    print("Running...")
+    httpd.serve_forever()
