ae9d207798ce088ff37d51ab9278aa1ed83e9b14
Lee816 <lgha0816@naver.com>
2023-06-21 22:08:33 +0900

    polls detail.html 템플릿 추가

--- My_Django/polls/views.py
+++ My_Django/polls/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 from .models import Question, Choice
 
 # Create your views here.
@@ -11,13 +13,26 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 
-def detail(request):
-    pass
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
-def vote(request):
-    pass
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {"question": question, "error_message": "항목을 선택하지 않았습니다."},
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
 
-def results(request):
+def results(request, question_id):
     pass
