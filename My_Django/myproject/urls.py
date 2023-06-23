fa8994d7335e6029fa12d742ce296fb13494bd8a
Lee816 <lgha0816@naver.com>
2023-06-23 12:32:33 +0900

    base.html 을 메인페이지로 사용

--- My_Django/myproject/urls.py
+++ My_Django/myproject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import Index
 
 urlpatterns = [
+    path("", Index),
     path("admin/", admin.site.urls),
     path("polls/", include("polls.urls")),
     path("books/", include("books.urls")),
