ceffa87c2297b38923a72c9e00b693a69d463765
Lee816 <lgha0816@naver.com>
2023-06-23 09:14:28 +0900

    books 앱 추가

--- My_Django/myproject/settings.py
+++ My_Django/myproject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     "polls.apps.PollsConfig",
+    "books.apps.BooksConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
